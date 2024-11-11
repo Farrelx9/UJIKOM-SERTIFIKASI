469a98b38960fc8a14fc9ed3632303459234375a
Farrelll <137156152+Farrelx9@users.noreply.github.com>
2024-11-11 23:15:18 +0700

    Initial Commit Cuy

---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login | HaloWin</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
+</head>
+
+<body id="bg-login">
+    <div class="box-login">
+        <h2>Login</h2>
+        <form action="" method="POST">
+            <input type="text" name="user" placeholder="Username" class="input-control">
+            <input type="password" name="pass" placeholder="Password" class="input-control">
+            <input type="submit" name="submit" value="Login" class="btn">
+        </form>
+        <?php
+        if (isset($_POST['submit'])) {
+            include 'db.php';
+            $user = $_POST['user'];
+            $pass = $_POST['pass'];
+
+            $cek = mysqli_query($conn, "SELECT * FROM tb_admin WHERE username = '" . $user . "' AND password = '" . MD5($pass) . "'");
+
+            if (mysqli_num_rows($cek) > 0) {
+                echo '<script>window.location="dashboard.php"</script>';
+            } else {
+                echo '<script>alert("Username atau Password Salah!")</script>';
+            }
+
+        }
+
+        ?>
+    </div>
+</body>
+
+</html>
